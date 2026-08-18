6335b03002b3c6dc0b94caeb99b525468977b958
Prafulla_Satle <prafull3000@gmail.com>
2026-08-18 12:27:21 +0530

    Change Profile image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { site } from "../data/site";
-import profilePlaceholder from "../assets/profile/ME.JPG";
+import profilePlaceholder from "../assets/profile/warmwhitebg.png";
 
 export default function Hero() {
   const scrollTo = (id: string) => {
